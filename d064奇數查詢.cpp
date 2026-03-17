99c192b2eff367209c13d6fc542e3a877ab0ffef
s1130174-lab <s1130174@dmhs.kh.edu.tw>
2026-03-17 11:27:10 +0800

    Create d064奇數查詢.cpp

---
+++ d064奇數查詢.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    cin>>i;
+    
+    if(1 % 2)==1){
+    	cout<<"odd"<<endl;
+    }else {
+    	cout<<"even"<<endl;
+    }
+    return 0;
+}
