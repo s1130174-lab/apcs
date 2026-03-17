5dab0b90b2c9f277eac73afd8b44df97b8a30f32
s1130174-lab <s1130174@dmhs.kh.edu.tw>
2026-03-17 11:36:11 +0800

    Create a003 兩光法師占卜術.cpp

---
+++ a003 兩光法師占卜術.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int M,D,S;
+    cin>>M>>D;
+	S=(M*2+D)%3;
+	if(S==2){
+		cout<<"大吉"<<endl;
+	}else  if(S==1){
+		cout<<"吉"<<endl;
+	}else{
+		cout<<"普通"<<endl;
+	}
+    return 0;
+}
