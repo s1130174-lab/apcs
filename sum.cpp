180e715917fae7a9871e598aaf1365fb85268156
s1130174-lab <s1130174@dmhs.kh.edu.tw>
2026-04-14 10:55:04 +0800

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int x,y;
+    cin >> x >> y;
+    while (x % y =0){
+          int temp=x % y;
+          x=y
+          y=temp;
+    }
+    cout << y << endl;
+    return 0;
